74eb1e63e51c851b31ff63555eb80b7a8059b1f4
vmanriquec <america78>
2018-10-16 23:14:37 -0500

    form

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 import java.util.*;
 import java.io.*;
 public class Main {
-public static Double tarifa;
+public static Double sueldobruto,tarifa,totalsueldosbrutoa=0.0,totalsueldosbrutob,totalsueldosbrutoc,totalsueldosbrutod,getTotalsueldosbrutototal=0.0;
+public  static int totaltrabajadoresa,totaltrabajadoresb,totaltrabajadoresc,totaltrabajadoresd;
+    public  static int totalhorastrabajadasa,totalhorastrabajadasb,totalhorastrabajadasc,totalhorastrabajadasd;
+
+
     public static void main(String[] args)throws IOException
     {  double ca;
 
@@ -10,46 +14,60 @@ public static Double tarifa;
 
         String cantidadcategoria;
         BufferedReader bufer= new BufferedReader(new InputStreamReader(System.in));
-
-
         cantidadcategoria=bufer.readLine();
-
         System.out.println("ingresar horas a trabajar");
-
-
         BufferedReader bufert= new BufferedReader(new InputStreamReader(System.in));
-
-
         ca=Double.valueOf( bufert.readLine());
         fff(cantidadcategoria,ca);
-
 }
-
     public   static String fff(String t,Double g){
 
         switch(t){
 
             case "a":
-                 tarifa = 45.0;
                 System.out.println("ingresar horas a trabajar");
-
-
+                cata(g);
                 break;
 
 
             case "b":
-
+                tarifa = 42.5;
+                sueldobruto=tarifa *g;
+                totalsueldosbrutob=sueldobruto+totalsueldosbrutob;
 
                 break;
 
+            case "c":
+                tarifa = 40.0;
+                sueldobruto=tarifa *g;
+                totalsueldosbrutoc=sueldobruto+totalsueldosbrutoc;
+
+                break;
+
+            case "d":
+                tarifa = 37.5;
+                sueldobruto=tarifa *g;
+                totalsueldosbrutod=sueldobruto+totalsueldosbrutod;
+
+                break;
 
             default:   System.out.println("categoria no existe");
 
         }
-        System.out.println("categoria"+t+"!!!!!"+"hirastrabajadas"+Double.toString(g));
+        System.out.println("categoria"+t+"!!!!!"+"hirastrabajadas"+Double.toString(g)+"total"+totalsueldosbrutoa);
 
 
 
         return null;
+    }
+    public  static Double cata(Double a){
+Double s = 0.0;
+
+        tarifa = 45.0;
+
+        sueldobruto=tarifa *a;
+        totalsueldosbrutoa=sueldobruto+totalsueldosbrutoa;
+
+return  s;
     }
 }
